d03f00b3e83c4c744acf315e36835eea611fcfab
nikita gavrilov <nikita.gavrilov@nordclan.com>
2024-01-06 16:17:41 +0300

    add login and signup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,56 @@
-import { defineConfig, type Plugin } from 'vite'
-import { resolve } from 'path'
-import handlebars from 'vite-plugin-handlebars'
+import { defineConfig, type Plugin } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [handlebars({
-    partialDirectory: resolve('src/components')
-  }) as unknown as Plugin]
-})
+  plugins: [
+    handlebars({
+      partialDirectory: resolve('src/components'),
+      context: {
+        login: {
+          inputs: [
+            {
+              type: 'text',
+              placeholder: 'Логин',
+              name: 'login'
+            },
+            {
+              type: 'password',
+              placeholder: 'Пароль',
+              name: 'password'
+            }
+          ]
+        },
+        signup: {
+          inputs: [
+            {
+              type: 'text',
+              placeholder: 'Имя',
+              name: 'first_name'
+            },
+            {
+              type: 'text',
+              placeholder: 'Фамилия',
+              name: 'second_name'
+            },
+            {
+              type: 'text',
+              placeholder: 'Логин',
+              name: 'login'
+            },
+            {
+              type: 'email',
+              placeholder: 'E-mail',
+              name: 'email'
+            },
+            {
+              type: 'password',
+              placeholder: 'Номер телефона',
+              name: 'phone'
+            }
+          ]
+        }
+      }
+    }) as unknown as Plugin
+  ]
+});
