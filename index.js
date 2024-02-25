f5bdc8561d79c060379ed4ec627102a70fb48846
nikita gavrilov <nikita.gavrilov@nordclan.com>
2024-02-25 18:33:27 +0300

    small changes

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const pageLinks = document.querySelectorAll('.page-link');
 
 pageLinks.forEach((link) => {
   link.addEventListener('click', (event) => {
-    event.preventDefault(); // Предотвращаем переход по ссылке
+    event.preventDefault();
 
     const href = link.getAttribute('href');
     console.log('href', href)
 
-    fetch(href) // Загружаем содержимое страницы с помощью fetch
+    fetch(href)
       .then((response) => response.text())
       .then((html) => {
         console.log('html', html);
         document.body.innerHTML = html;
-        // Обновляем URL в адресной строке без перезагрузки страницы
         history.pushState(null, null, href);
       })
       .catch((error) => console.error('Ошибка загрузки страницы:', error));
