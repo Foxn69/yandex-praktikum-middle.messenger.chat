15324295f5ef50860749f535cd32f6010794358e
nikita gavrilov <nikita.gavrilov@nordclan.com>
2024-03-05 21:49:51 +0300

    update server return index.hrml

--- server/server.js
+++ server/server.js
@@ -21,9 +21,9 @@ console.log('======>', path.join(__dirname, '../dist'));
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
-//   });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/src', 'index.html'));
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening ${PORT} port`);
